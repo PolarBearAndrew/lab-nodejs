3433c6d6cf47112b931b33d92773aa6d5ad3a8bf
AndrewChen <chenpoanandrew@gmail.com>
2022-07-22 21:24:05 -0500

    chore: update perf demo.

--- 002-object-operating/perf/001.js
+++ 002-object-operating/perf/001.js
@@ -7,7 +7,9 @@ function randStr(len) {
     .substr(0, len);
 }
 
-function case1() {
+// origin_use_case: 592.171ms
+// simulate using date string YYYY-MM-DD as key
+function origin_use_case() {
   const keys = [];
   const object = {};
 
@@ -15,7 +17,7 @@ function case1() {
     keys.push(randStr(10));
   }
 
-  console.time('case1');
+  console.time('origin_use_case');
 
   for (let i = 0; i < len; ++i) {
     object[keys[i]] = 1;
@@ -24,10 +26,12 @@ function case1() {
   for (let i = 0; i < len; ++i) {
     object[keys[i]] = object[keys[i]] + 1;
   }
-  console.timeEnd('case1');
+  console.timeEnd('origin_use_case');
 }
 
-function case2() {
+// better_practice: 29.859ms
+// we found out using array index as key performance is much better
+function better_practice() {
   const keys = [];
   const object = {};
 
@@ -36,7 +40,7 @@ function case2() {
     keys.push(i.toString());
   }
 
-  console.time('case2');
+  console.time('better_practice');
 
   for (let i = 0; i < len; ++i) {
     object[keys[i]] = 1;
@@ -45,30 +49,12 @@ function case2() {
   for (let i = 0; i < len; ++i) {
     object[keys[i]] = object[keys[i]] + 1;
   }
-  console.timeEnd('case2');
+  console.timeEnd('better_practice');
 }
 
-function case3() {
-  const keys = [];
-  const object = {};
-
-  for (let i = 0; i < len; ++i) {
-    keys.push(`i${i}`);
-  }
-
-  console.time('case3');
-
-  for (let i = 0; i < len; ++i) {
-    object[keys[i]] = 1;
-  }
-
-  for (let i = 0; i < len; ++i) {
-    object[keys[i]] = object[keys[i]] + 1;
-  }
-  console.timeEnd('case3');
-}
-
-function case4() {
+// better_practice_but_not_working_case: 1.029s
+// when number of index have huge gap, then perf will drop back to the original one
+function better_practice_but_not_working_case() {
   const keys = [];
   const object = {};
 
@@ -76,7 +62,7 @@ function case4() {
     keys.push((i * 10).toString());
   }
 
-  console.time('case4');
+  console.time('better_practice_but_not_working_case');
 
   for (let i = 0; i < len; ++i) {
     object[keys[i]] = 1;
@@ -85,12 +71,41 @@ function case4() {
   for (let i = 0; i < len; ++i) {
     object[keys[i]] = object[keys[i]] + 1;
   }
-  console.timeEnd('case4');
+  console.timeEnd('better_practice_but_not_working_case');
+}
+
+// other_case: 585.931ms
+// if add a prefix, than the index won't work anymore
+function other_case() {
+  const keys = [];
+  const object = {};
+
+  for (let i = 0; i < len; ++i) {
+    keys.push(`i${i}`);
+  }
+
+  console.time('other_case');
+
+  for (let i = 0; i < len; ++i) {
+    object[keys[i]] = 1;
+  }
+
+  for (let i = 0; i < len; ++i) {
+    object[keys[i]] = object[keys[i]] + 1;
+  }
+  console.timeEnd('other_case');
 }
 
 console.log('\nfor-loop times:', len);
 
-case1();
-case2();
-case3();
-case4();
+origin_use_case();
+better_practice();
+better_practice_but_not_working_case();
+other_case();
+
+// output:
+// for-loop times: 1000000
+// origin_use_case: 620.368ms
+// better_practice: 30.528ms
+// better_practice_but_not_working_case: 995.774ms
+// other_case: 562.406ms
